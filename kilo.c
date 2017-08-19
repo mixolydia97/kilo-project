2eefb0fbaabe7886fb9cd42f2075eb5cbbeb92ca
Mixolydia97 <sarah.randall@mail.mcgill.ca>
2017-08-19 16:48:42 -0400

    null callback?

--- kilo.c
+++ kilo.c
@@ -69,7 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -378,7 +378,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
 	if (E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if (E.filename == NULL) {
 			editorSetStatusMessage("Save aborted");
 			return;
@@ -409,7 +409,7 @@ void editorSave() {
 /*** find ***/
 
 void editorFind() {
-	char *query = editorPrompt("Search: %s (ESC to cancel)");
+	char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
 	if (query == NULL) return;
 
 	int i;
@@ -570,7 +570,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 
@@ -586,11 +586,13 @@ char *editorPrompt(char *prompt) {
 			if (buflen != 0) buf[--buflen] = '\0';
 		} else if (c == '\x1b') {
 			editorSetStatusMessage("");
+			if (callback) callback(buf, c);
 			free(buf);
 			return NULL;
 		} else if (c == '\r') {
 			if (buflen != 0) {
 				editorSetStatusMessage("");
+				if (callback) callback(buf, c);
 				return buf;
 			}
 		} else if (!iscntrl(c) && c < 128) {
@@ -601,6 +603,8 @@ char *editorPrompt(char *prompt) {
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+
+		if (callback) callback(buf, c);
 	}
 }
 
