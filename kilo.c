e78ae67c6f3d926d8400780dbbe4eae9b8521cf8
Mixolydia97 <sarah.randall@mail.mcgill.ca>
2017-09-01 11:37:10 -0400

    rendering for nonprinting character

--- kilo.c
+++ kilo.c
@@ -737,7 +737,22 @@ void editorDrawRows(struct abuf *ab) {
 			int current_color = -1;
 			int j;
 			for (j = 0; j < len; j++) {
-				if (hl[j] == HL_NORMAL) {
+
+				//rendering nonprintable characters
+				if (iscntrl(c[j])) {
+					char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+					abAppend(ab, "\x1b[7m", 4);
+					abAppend(ab, &sym, 1);
+					abAppend(ab, "\x1b[m", 3);
+
+					//correct the highlight for chars after nonprintable symbols
+					if (current_color != -1) {
+						char buf[16];
+						int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", 
+								current_color);
+						abAppend(ab, buf, clen);
+					}
+				} else if (hl[j] == HL_NORMAL) {
 					if (current_color != -1) {
 						abAppend(ab, "\x1b[39m", 5);
 						current_color = -1;
